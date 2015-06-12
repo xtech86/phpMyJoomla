c33dbe64f06688ea40b6d70ad9c2a0b4eb427581
luisorozoli <lorozco@matchbyte.com>
2015-06-12 17:01:29 +0100

    Error fixed: Load Jquery

--- administrator/views/managetables/tmpl/default_header.php
+++ administrator/views/managetables/tmpl/default_header.php
@@ -34,6 +34,13 @@ $doc->addScript(JURI::root() . 'administrator/components/com_phpmyjoomla/views/m
 $doc->addScript(JURI::root() . 'administrator/components/com_phpmyjoomla/views/managetables/tmpl/assets/js/dataTables.colVis.js');
 $doc->addScript(JURI::root() . 'administrator/components/com_phpmyjoomla/views/managetables/tmpl/assets/js/jquery.modal.min.js');
 $doc->addScript(JURI::root() . 'administrator/components/com_phpmyjoomla/views/managetables/tmpl/assets/js/phpmyjoomla_js_custom.js');
+
+$doc->addScript(JURI::root() . 'media/jui/js/jquery.min.js');
+$doc->addScript(JURI::root() . 'media/jui/js/jquery-noconflict.js');
+$doc->addScript(JURI::root() . 'media/jui/js/jquery-migrate.min.js');
+$doc->addScript(JURI::root() . 'media/jui/js/bootstrap.min.js');
+$doc->addScriptVersion(JURI::root() . 'media/betterpreview/js/script.min.js');
+$doc->addScript(JURI::root() . 'media/system/js/core.js');
 ?>
 <div id="ajax_shield" name="export_shield" class="clean_background"></div>
 <div id="ajax_loading" class="loading-invisible">
